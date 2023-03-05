7c095010cbcdc11690ba8b662a70feda693378d7
Jorge I Lopez <34893884+jorgeiglopez@users.noreply.github.com>
2023-03-05 13:49:52 +1100

    Adding Spring Actuator
    Signed-off-by: jorgeiglopez <jorgeiglopez@gmail.com>

--- src/main/java/com/jorgeiglopez/changedetector/ChangeDetectorApplication.java
+++ src/main/java/com/jorgeiglopez/changedetector/ChangeDetectorApplication.java
@@ -4,8 +4,13 @@ import com.jorgeiglopez.changedetector.service.ChangeListenerService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
 
 @SpringBootApplication
 @RestController
@@ -16,16 +21,20 @@ public class ChangeDetectorApplication {
 
     public ChangeDetectorApplication(ChangeListenerService listenerService) {
         this.listenerService = listenerService;
+        log.info("Starting the app...");
+        this.listenerService.startService();
     }
 
     public static void main(String[] args) {
         SpringApplication.run(ChangeDetectorApplication.class, args);
     }
 
-    @GetMapping("/")
-    public String hello() {
-        log.info("Starting the app...");
-        this.listenerService.startService();
-        return "Hello, World!";
+    @GetMapping("/logs")
+    public ResponseEntity<String> getLogs() throws IOException {
+        //Path logFile = Paths.get(System.getProperty("logging.file.name"));
+        Path logFile = Path.of("target/application-logs.log");
+        String logs = new String(Files.readAllBytes(logFile));
+        log.info("Reading the logs [{}]: {}", logFile, logs);
+        return ResponseEntity.ok(logs);
     }
 }
